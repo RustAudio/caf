0f7fd78bdd6719707ebd9f0854e5bfe0fdb78516
est31 <MTest31@outlook.com>
2017-01-29 15:53:20 +0100

    Impl some common traits for the error type

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,8 @@
 
 use std::string::FromUtf8Error;
 use std::io::{Error as IoError};
+use std::error::Error;
+use std::fmt::Display;
 use ::ChunkType;
 
 #[derive(Debug)]
@@ -29,5 +31,38 @@ impl From<IoError> for CafError {
 impl From<FromUtf8Error> for CafError {
 	fn from(utf8_err :FromUtf8Error) -> Self {
 		CafError::FromUtf8(utf8_err)
+	}
+}
+
+impl Error for CafError {
+	fn description(&self) -> &str {
+		use CafError::*;
+		match self {
+			&Io(_) => "IO error",
+			&FromUtf8(_) => "Can't decode UTF-8",
+			&NotCaf => "The given stream doesn't start with a CAF header",
+			&UnsupportedChunkType(_) => "Encountered a chunk with an unsupported type",
+		}
+	}
+
+	fn cause(&self) -> Option<&Error> {
+		use CafError::*;
+		match self {
+			&Io(ref err) => Some(err as &Error),
+			&FromUtf8(ref err) => Some(err as &Error),
+			_ => None
+		}
+	}
+}
+
+impl Display for CafError {
+	fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
+		use CafError::*;
+		match *self {
+			Io(ref err) => err.fmt(f),
+			FromUtf8(ref err) => err.fmt(f),
+			UnsupportedChunkType(_) |
+			NotCaf => write!(f, "{}", self.description()),
+		}
 	}
 }
